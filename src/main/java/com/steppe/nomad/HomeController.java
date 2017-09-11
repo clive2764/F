4e1d50d286e133ec349211a7782ffc7efdb3b46f
pc-PC\pc <wlgns27647@gmail.com>
2017-09-11 14:15:16 +0900

    최종본

---
+++ src/main/java/com/steppe/nomad/HomeController.java
@@ -0,0 +1,124 @@
+package com.steppe.nomad;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.steppe.nomad.bean.Member;
+import com.steppe.nomad.service.EmailManagement;
+import com.steppe.nomad.service.MemberManagement;
+
+@Controller
+@SessionAttributes("member")
+public class HomeController {
+
+	private ModelAndView mav;
+	private EmailManagement em;
+
+	@Autowired
+	private MemberManagement mm;	//페이지 처음 진입용
+	@RequestMapping(value = "/")
+	public ModelAndView home(Model model) {
+		mav = new ModelAndView();
+		mav.setViewName("home"); //main.jsp 메인페이지
+		return mav;
+	}
+	//상단 헤더메뉴의 메인페이지 링크용
+	@RequestMapping(value = "/main")
+	public ModelAndView home2(Model model) {
+		mav = new ModelAndView();
+		mav.setViewName("home"); //main.jsp 메인페이지
+		return mav;
+	}
+
+	//회원가입용
+	@RequestMapping(value = "/goJoin")
+	public ModelAndView goJoin() {
+		System.out.println("회원가입 페이지로 이동");
+		mav = new ModelAndView();
+		mav.setViewName("join"); //join.jsp 회원가입 페이지
+		return mav;
+	}
+	@RequestMapping(value="/goProject")
+	public ModelAndView goProject(){
+		mav = new ModelAndView();
+		mav.setViewName("project"); //join.jsp 회원가입 페이지
+		return mav;
+	}
+	@RequestMapping(value="/goProjectDetail")
+	public ModelAndView goProjectDetail(){
+		mav = new ModelAndView();
+		mav.setViewName("projectDetail"); //projectDetail.jsp 프로젝트 상세보기 페이지
+		return mav;
+	}
+	@RequestMapping(value="/goAddProject")
+	public ModelAndView goAddProject(){
+		mav = new ModelAndView();
+		mav.setViewName("projectInsert"); //projectDetail.jsp 프로젝트 상세보기 페이지
+		return mav;
+	}
+
+	//이메일 인증
+	@RequestMapping(value = "/sendCode")
+	public ModelAndView sendCode() {
+		System.out.println("ModelAndView sendCode 시작");
+		mav = new ModelAndView(); 
+		mm.sendCode();
+		return mav;
+	}
+
+	
+	//로그인 페이지 이동
+	@RequestMapping(value = "/goLogin", method = RequestMethod.GET)
+	public ModelAndView goLogin() {
+		System.out.println("로그인 페이지로 이동");
+		mav = new ModelAndView();
+		mav.setViewName("login"); //login.jsp 로그인 페이지
+		return mav;
+	}
+	
+	//로그인
+	@RequestMapping(value = "/access", method = RequestMethod.POST)
+	public ModelAndView access(Member mb) {
+		System.out.println("mb="+mb.getM_id());
+		mav = mm.execute(mb,1);
+		return mav;
+	}
+	
+	//로그아웃
+	   @RequestMapping(value = "/logout")
+	   public ModelAndView logout(Member mb) {
+	      System.out.println("로그아웃");
+	      mav = mm.execute(mb,2);
+	      return mav;
+	   }
+	
+	
+	@RequestMapping(value = "/fstMm", method = RequestMethod.GET)
+
+	public String fstMm(Model model) {
+		return "fstMm";
+	}
+	
+	//이용방법 페이지로 이동
+	@RequestMapping(value= "/goManual")
+	public ModelAndView goManual(){
+		System.out.println("이용방법 페이지로 이동");
+		mav = new ModelAndView();
+		mav.setViewName("manual");
+		return mav;
+	}
+	
+	@RequestMapping(value= "/goNotice")
+	public ModelAndView goNotice(){
+		System.out.println("공지사항 페이지로 이동");
+		mav = new ModelAndView();
+		mav.setViewName("notice");
+		return mav;
+	}
+	
+}
---
+++ src/main/java/com/steppe/nomad/service/MemberManagement.java
@@ -0,0 +1,194 @@
+package com.steppe.nomad.service;
+
+import java.io.IOException;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.steppe.nomad.bean.Member;
+import com.steppe.nomad.dao.MemberDao;
+
+@Repository 
+public class MemberManagement {
+	@Autowired
+	private HttpSession session;//request.getSession();
+
+	@Autowired
+	private HttpServletRequest request;
+
+	@Autowired
+	private HttpServletResponse response;
+
+	//싱글톤
+	@Autowired
+	private MemberDao mDao;
+
+	private ModelAndView mav;
+
+	public ModelAndView execute(Member mb, int cmd){
+		switch (cmd) {
+		case 1:
+			memberAccess(mb);//로그인
+			break;
+		case 2:
+			logout(mb);//로그아웃
+			break;
+		case 3:
+			//memberInsert(mb);//회원가입
+			break;
+		default:
+			break;
+		}
+		return mav;
+
+	}
+
+	private void logout(Member mb) {
+		mav = new ModelAndView();
+		String view = null;
+		System.out.println(session.getAttribute("m_id"));
+		if(session !=null){
+			session.invalidate();
+			System.out.println("session="+session);
+			System.out.println("세션 제거 완료");
+			System.out.println(session.getAttribute("m_id"));
+		}	
+		view = "home";
+		mav.setViewName(view);
+		
+	}
+	private ModelAndView memberAccess(Member mb) {
+		mav=new ModelAndView();
+		String view=null;
+		
+		String m_id=request.getParameter("m_id");
+		String m_pass=request.getParameter("m_pw");
+		
+		String passC=mDao.getPwd(m_id);
+		System.out.println(m_pass);
+		System.out.println(passC);
+		if(m_pass!=null){
+			if(m_pass.equals(passC)){
+				
+				String m_kind=mDao.getKind(m_id);
+				session.setAttribute("m_kind", m_kind);
+				session.getAttribute("m_kind");
+				
+				System.out.println("로그인성공");
+				session.setAttribute("m_id",m_id);
+				session.getAttribute("m_id");
+				
+				session.setAttribute("m_pw", m_pass);
+				session.getAttribute("m_pw");
+				
+				session.setAttribute("member", mb);
+				session.getAttribute("member");
+
+				System.out.println(mDao.getKind(m_id));
+				System.out.println("아이디:"+session.getAttribute("m_id"));
+				System.out.println("패스워드:"+session.getAttribute("m_pw"));
+				System.out.println("회원종류:"+session.getAttribute("m_kind"));
+				System.out.println("세션:"+session.getId());
+
+				view="redirect:/";
+				mav.setViewName(view);
+				
+			}
+			else{
+				view="login";
+				mav.setViewName(view);
+			}
+		}
+		return mav;
+		
+	}
+	public void sendCode() {
+		System.out.println("mm.sendCode() 시작");
+		MemberDao dao = new MemberDao();
+
+		int flag = Integer.valueOf(request.getParameter("flag"));
+		System.out.println("flag: "+flag);
+		String email = request.getParameter("userEmail");
+		String msgEmail;
+		String msgSend = "";
+
+		if(flag == 1)
+		{
+			// 중복 이메일 체크
+
+			if(dao.isEmail(email))
+				msgEmail = "중복된 이메일이 있습니다.";
+			else
+			{
+				msgEmail = "";
+				msgSend = "이메일의 인증번호를 확인해주세요.";
+
+				// 인증번호 생성
+
+				Random random = new Random();
+				int code = random.nextInt(1000000) + 100000;
+
+				if(code > 1000000)
+					code = code - 100000;
+
+				// 메일 보내기
+
+				EmailManagement mail = new EmailManagement();
+				mail.sendCode(email, code);
+
+				HttpSession session = request.getSession();
+				session.setAttribute("code", code);
+			}
+		}
+		else
+		{
+			if(!dao.isEmail(email))
+				msgEmail = "가입된 이메일 정보가 없습니다.";
+			else
+			{
+				msgEmail = "";
+				msgSend = "이메일의 인증번호를 확인해주세요.";
+
+				// 인증번호 생성
+
+				Random random = new Random();
+				int code = random.nextInt(1000000) + 100000;
+
+				if(code > 1000000)
+					code = code - 100000;
+
+				// 메일 보내기
+
+				EmailManagement mail = new EmailManagement();
+				mail.sendCode(email, code);
+
+				HttpSession session = request.getSession();
+				session.setAttribute("code", code);
+			}
+		}
+
+		try 
+		{
+			response.getWriter().write(msgEmail + msgSend);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+
+		}
+
+		//dao.close();
+
+	}
+
+
+
+}
